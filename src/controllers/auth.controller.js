82004d5efdb9c63fd3510f88c8197ac5669fbe8a
Administrator <admin@cixio.com>
2026-02-05 16:35:09 +0530

    remove hardcode

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -58,7 +58,7 @@ const register = async (req, res) => {
         
         // Send notification to support team - background (non-blocking)
         sendEmail({
-            to: process.env.SUPPORT_EMAIL || 'support@cixio.com',
+            to: process.env.SUPPORT_EMAIL,
             subject: 'New User Registration',
             html: `
                 <!DOCTYPE html>
